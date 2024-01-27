8fcd7a38352d07c60e780033da76089ce06245f0
j10h8 <john.hallbeck@outlook.com>
2024-01-27 22:55:31 +0100

    Establish connection to storage account and test upload functionality

---
+++ src-frontend/Services/DataService.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Storage.Blobs;
+using System.Text.Json;
+
+namespace src_frontend.Services
+{
+    public class DataService
+    {
+        BlobServiceClient blobServiceClient;
+        BlobContainerClient containerClient;
+
+        public DataService()
+        {
+            blobServiceClient = new BlobServiceClient("storageAccount");
+            containerClient = blobServiceClient.GetBlobContainerClient("blobs");
+        }
+
+        public bool AddToStorage(string data)
+        {
+            string json = JsonSerializer.Serialize(data);
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(json);
+
+            string blobName = "blob_" + Guid.NewGuid().ToString() + ".json";
+
+            using (MemoryStream memoryStream = new MemoryStream(byteArray))
+            {
+                if (containerClient.UploadBlob(blobName, memoryStream).Value != null)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
+}
