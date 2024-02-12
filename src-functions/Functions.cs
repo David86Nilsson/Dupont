8d5fc0d5d10cd2fe52ffa181226c88c20ceef293
j10h8 <john.hallbeck@outlook.com>
2024-02-12 13:44:32 +0100

    Connect pieces, remove comments and unused methods

--- src-functions/Functions.cs
+++ src-functions/Functions.cs
@@ -4,13 +4,9 @@ using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using src_functions.Models;
 using System.Text;
 using JsonSerializer = System.Text.Json.JsonSerializer;
-
-
 
 namespace src_functions
 {
@@ -20,7 +16,6 @@ namespace src_functions
         {
             public string EmailAddress { get; set; }
             public string Subject { get; set; }
-            // Add other properties as needed
         }
 
         public class EventData
@@ -36,9 +31,6 @@ namespace src_functions
             public EventData data { get; set; }
         }
 
-
-
-
         private readonly ILogger<Functions> _logger;
 
         public Functions(ILogger<Functions> logger)
@@ -46,15 +38,12 @@ namespace src_functions
             _logger = logger;
         }
 
-
-
         [Function("BlobTrigger")]
         public async Task BlobTrigger([BlobTrigger("blobs/{name}", Connection = "connection")] Stream stream, string name)
         {
             using var blobStreamReader = new StreamReader(stream);
             var content = await blobStreamReader.ReadToEndAsync();
 
-            // TODO: Skapa event och skicka till EventGrid
             _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
 
             string endpoint = Environment.GetEnvironmentVariable("TopicEndpoint") ?? "";
@@ -69,34 +58,18 @@ namespace src_functions
                 content
             );
             client.SendEventAsync(ev).GetAwaiter().GetResult();
-            _logger.LogInformation($"Event sent"); // logger
+            _logger.LogInformation($"Event sent");
         }
-
-
-
-        //[Function("EventTrigger")]
-        //public async Task EventTrigger([EventGridTrigger] string eventGridEvent)
-        //{
-        //    // TODO Gör något när EventGrid får ett event
-        //    _logger.LogInformation($"C# EventGrid trigger function processed an event: {eventGridEvent}");
-        //}
-
-
-
 
         [Function("EventTrigger")]
         public async Task EventTrigger([EventGridTrigger] string eventGridEvent)
         {
-            // TODO Gör något när EventGrid får ett event
             _logger.LogInformation($"C# EventGrid trigger function processed an event: {eventGridEvent}");
-
 
             try
             {
-                // Use the raw eventGridEvent as a string
                 string messageContent = eventGridEvent;
 
-                // Send the message to Service Bus asynchronously
                 await SendMessageToServiceBusAsync(messageContent);
 
                 _logger.LogInformation($"Message sent to Service Bus: {messageContent}");
@@ -105,15 +78,12 @@ namespace src_functions
             {
                 _logger.LogError($"Error processing event: {ex.Message}");
             }
-
         }
-
-
 
         private async Task SendMessageToServiceBusAsync(string messageContent)
         {
 
-            string connectionString = "Endpoint=sb://sb-dupont.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=xx6KMSGW4M1uxU2DwhdY4Y0GijmozD07k+ASbNrzq0c=";
+            string connectionString = Environment.GetEnvironmentVariable("sbConnectionString");
             string topicName = "sb-users";
 
             var client = new ServiceBusClient(connectionString);
@@ -121,9 +91,7 @@ namespace src_functions
 
             try
             {
-
                 var message = new ServiceBusMessage(messageContent);
-
 
                 await sender.SendMessageAsync(message);
             }
@@ -133,10 +101,7 @@ namespace src_functions
             }
             finally
             {
-
                 await sender.CloseAsync();
-
-
             }
         }
 
@@ -149,51 +114,7 @@ namespace src_functions
         {
             try
             {
-
                 _logger.LogInformation($"C# ServiceBus topic trigger function processed message. {message.Body}");
-
-                //parsing bson
-
-                //string bsonLikeString = Encoding.UTF8.GetString(message.Body);
-
-
-                //BsonDocument bsonDocument = BsonDocument.Parse(bsonLikeString);
-
-
-                //string jsonData = bsonDocument["data"].AsString;
-
-
-                //BsonDocument nestedDocument = BsonDocument.Parse(jsonData);
-
-
-                //string emailAddress = nestedDocument["EmailAddress"].AsString;
-
-
-                //_logger.LogInformation($"C# EmailAddress: {emailAddress}");
-
-
-
-
-
-
-
-
-
-                //EventMessage eventMessage = JsonConvert.DeserializeObject<EventMessage>(jsonString);
-
-
-                //string emailAddress = eventMessage.data.EmailAddress;
-                //_logger.LogInformation($"C# EmailAddress: {emailAddress}");
-
-
-
-
-
-                //string jsonString = Encoding.UTF8.GetString(message.Body);
-
-                //string formattedMessage = message.Body.ToString().Replace("\\", "");
-
-                //_logger.LogInformation(formattedMessage);
 
                 ServiceBusDataModel? serviceBusDataModel = System.Text.Json.JsonSerializer.Deserialize<ServiceBusDataModel>(message.Body);
                 if (serviceBusDataModel == null)
@@ -216,104 +137,9 @@ namespace src_functions
 
                 _logger.LogInformation($"C# ServiceBus topic trigger extracted EmailAddress - {emailDataModel.EmailAddress}");
 
-                //var emailAddress = message.Body.ToObjectFromJson();
+                SaveEmailToCosmosDB(emailDataModel.EmailAddress, _logger);
 
-
-                //JObject jsonObject = JObject.Parse(jsonString);
-
-
-                //JObject dataObject = JObject.Parse((string)jsonObject["id"]);
-
-                //string dataObject = (string)jsonObject["data"];
-
-                //string email = (string)jsonObject["data"];
-
-                //JObject emailObject = JObject.Parse(dataObject);
-
-                //string emailAddress = (string)jsonObject["EmailAddress"];
-
-
-
-                //_logger.LogInformation($"C# EmailAddress: {emailAddress}");
-
-
-
-
-
-
-
-
-                //string cosmosEndpoint = "https://db-dupont.documents.azure.com:443/";
-                //string cosmosKey = "6Z9o0FOJpR4mRZpMIImbq0wDRXBEZzjxHczrQHMHbKzpG6j3R6ZyWzPJWoOmLTJpqjlytj6IqsUcACDbGCSQ7Q==";
-                //string databaseName = "dupont-database";
-                //string containerName = "dupont-container";
-
-                //CosmosClient cosmosClient = new CosmosClient(cosmosEndpoint, cosmosKey);
-                //var database = cosmosClient.GetDatabase(databaseName);
-                //var container = database.GetContainer(containerName);
-
-
-
-
-                //var document = new
-                //{
-                //    id = Guid.NewGuid().ToString(),
-                //    Email = email
-                //};
-
-                //var result = container.CreateItemAsync(document).Result;
-
-
-
-
-
-
-
-
-
-                //var document = new
-                //{
-                //    id = Guid.NewGuid().ToString(),
-                //    messageId = message.MessageId,
-                //    content = messageContent,
-                //    // Add other properties as needed
-                //};
-
-                //// Save the document to Cosmos DB
-                //await container.CreateItemAsync(document);
-
-
-
-
-
-
-
-
-
-
-
-                // Convert the message body to a string
-                //string messageBody = Encoding.UTF8.GetString(message.Body);
-
-                //_logger.LogInformation($"C# Converted to string. {message.Body}");
-
-                ////// Extract email address from the message
-                //string emailAddress = ExtractEmailAddress(messageBody);
-
-                //if (!string.IsNullOrEmpty(emailAddress))
-                //{
-                //    _logger.LogInformation($"Extracted email address: {emailAddress}");
-
-                //    // Save email address to Cosmos DB or perform other actions
-                //    //SaveEmailAddressToCosmosDB(emailAddress);
-                //}
-                //else
-                //{
-                //    _logger.LogWarning("Email address not found in the message.");
-                //}
-
-
-
+                await TriggerLogicApp(await GetEmailModel(emailDataModel.EmailAddress));
             }
             catch (Exception ex)
             {
@@ -321,128 +147,12 @@ namespace src_functions
             }
         }
 
-
-
-
-
-
-
-        private string ExtractEmailAddress(string messageBody)
+        public void SaveEmailToCosmosDB(string email, ILogger log)
         {
-            try
-            {
-
-                JObject jsonObject = JObject.Parse(messageBody);
-
-
-
-                string emailAddress = (string)jsonObject["data"];
-                return emailAddress;
-
-
-            }
-            catch (JsonReaderException ex)
-            {
-
-                _logger.LogError(ex, "Error parsing JSON message body.");
-                return null;
-            }
-        }
-
-
-
-
-
-        //    [Function("ServiceBusTrigger")]
-        //    public void ServiceBusTrigger([ServiceBusTrigger(
-
-        //"sb-users",
-        //"sbs-dupont",
-        //Connection = "sbConnectionString")]string message)
-        //    {
-        //        try
-        //        {
-        //            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {message}");
-
-        //            // Extract email address from the message
-        //            string emailAddress = ExtractEmailAddress(message);
-
-        //            if (!string.IsNullOrEmpty(emailAddress))
-        //            {
-        //                _logger.LogInformation($"Extracted email address: {emailAddress}");
-
-        //                //// Save email address to Cosmos DB
-        //                //SaveEmailAddressToCosmosDB(emailAddress);
-        //            }
-        //            else
-        //            {
-        //                _logger.LogWarning("Email address not found in the message.");
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            _logger.LogError(ex, "Error processing Service Bus message.");
-        //        }
-
-
-
-        //    }
-
-        //    private string ExtractEmailAddress(string message)
-        //    {
-        //        
-
-        //        JObject innerObject = JObject.Parse(message);
-
-        //        
-        //        string emailAddress = (string)innerObject["data"];
-
-        //        //JObject outerObject = JObject.Parse(dataJsonString);
-
-        //        
-        //        //string emailAddress = (string)outerObject["EmailAddress"];
-
-
-
-
-
-        //        //string messageBody = Encoding.UTF8.GetString(message.Body);
-        //        
-        //        //JObject jsonObject = JObject.Parse(messageBody);
-
-        //        //string emailAddress = (string)jsonObject["EmailAddress"];
-
-        //        return emailAddress;
-        //    }
-
-
-
-
-
-
-        private static string ExtractEmailFromMessage(string message)
-        {
-
-            try
-            {
-                var jsonObject = JsonConvert.DeserializeObject<BusMessage>(message);
-                return jsonObject.EmailAddress;
-            }
-            catch (Newtonsoft.Json.JsonException)
-            {
-
-                return null;
-            }
-
-        }
-
-
-        private static void SaveEmailToCosmosDB(string email, ILogger log)
-        {
-            string cosmosEndpoint = "https://db-dupont.documents.azure.com:443/";
-            string cosmosKey = "6Z9o0FOJpR4mRZpMIImbq0wDRXBEZzjxHczrQHMHbKzpG6j3R6ZyWzPJWoOmLTJpqjlytj6IqsUcACDbGCSQ7Q==";
-            string databaseName = "dupont-database";
-            string containerName = "dupont-container";
+            string? cosmosEndpoint = Environment.GetEnvironmentVariable("CosmosEndpoint");
+            string? cosmosKey = Environment.GetEnvironmentVariable("CosmosPrimaryKey");
+            string? databaseName = Environment.GetEnvironmentVariable("CosmosDatabaseName");
+            string? containerName = Environment.GetEnvironmentVariable("CosmosContainerName");
 
             CosmosClient cosmosClient = new CosmosClient(cosmosEndpoint, cosmosKey);
             var database = cosmosClient.GetDatabase(databaseName);
@@ -458,13 +168,11 @@ namespace src_functions
             log.LogInformation($"Email saved to Cosmos DB. Request charge: {result.RequestCharge}");
         }
 
-
         public async Task<EmailModel> GetEmailModel(string emailAddress)
         {
             HttpClient httpClient = new HttpClient();
 
-            HttpResponseMessage response = await httpClient.GetAsync("https://www.thecocktaildb.com/api/json/v1/1/random.php");
-
+            HttpResponseMessage response = await httpClient.GetAsync(Environment.GetEnvironmentVariable("CocktailDbRequestUri"));
             if (response.IsSuccessStatusCode)
             {
                 string responseContent = await response.Content.ReadAsStringAsync();
@@ -510,11 +218,7 @@ namespace src_functions
 
             HttpContent httpContent = new StringContent(JsonSerializer.Serialize(emailModel), Encoding.UTF8, "application/json");
 
-            HttpResponseMessage response = await httpClient.PostAsync("https://prod-21.eastus.logic.azure.com:443/workflows/2b6ca9d6a77542208fc805f04963166e/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6BJWg7HTmwN_5L1oteeWNi9JEvNOQmt5GBu0SefuzdM", httpContent);
+            HttpResponseMessage response = await httpClient.PostAsync(Environment.GetEnvironmentVariable("LogicAppEndPoint"), httpContent);
         }
-
     }
 }
-
-
-
