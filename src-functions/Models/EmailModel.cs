9eba6f7e2bd27de3ad1ad48fa3f5abe290b376d1
j10h8helo <john.hallbeck@helo.se>
2024-02-13 10:38:04 +0100

    Add drink image

--- src-functions/Models/EmailModel.cs
+++ src-functions/Models/EmailModel.cs
@@ -4,6 +4,7 @@
     {
         public string? EmailAddress { get; set; }
         public string? DrinkName { get; set; }
+        public string? ImageUrl { get; set; }
         public List<string> DrinkIngredients { get; set; } = new List<string>();
     }
 }
